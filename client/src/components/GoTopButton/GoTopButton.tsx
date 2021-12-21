8403c4c783cd7e881dc17382421f45ed02b429e6
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-12-21 19:54:43 -0400

    Update

---
+++ client/src/components/GoTopButton/GoTopButton.tsx
@@ -0,0 +1,9 @@
+import { FaArrowUp } from "react-icons/fa";
+
+export default function GoTopButton() {
+  return (
+    <button type="button" title="Go to top">
+      <FaArrowUp />
+    </button>
+  );
+}
