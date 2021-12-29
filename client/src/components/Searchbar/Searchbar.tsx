b293455326dda50870ee3e35bb5c06d6acd4bf2b
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-12-29 09:12:36 -0400

    Update Searchbar.tsx

--- client/src/components/Searchbar/Searchbar.tsx
+++ client/src/components/Searchbar/Searchbar.tsx
@@ -1,14 +1,30 @@
 import styles from "./Searchbar.module.scss";
 import { FaSearch } from "react-icons/fa";
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Searchbar() {
+  const navigate = useNavigate();
+
   const [scrollPosition, setScrollPosition] = useState(window.scrollY);
+  const [input, setInput] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>  setInput(e.target.value);
+  
+  const resetInput = () => setInput("");
+  
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    resetInput();
+    navigate(`/search/${input}`);
+  };
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       setScrollPosition(window.scrollY);
     });
   }, []);
+
   return (
     <div
       className={
@@ -17,18 +33,21 @@ export default function Searchbar() {
           : styles.container
       }
     >
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
         <div className={styles.inputContainer}>
           <label htmlFor="search" className={styles.inputLabel}>
             Search a gif
           </label>
           <input
             autoComplete="off"
-            placeholder="Search all the GIFs and Stickers"
-            type="search"
-            name="search"
-            id="search"
             className={styles.input}
+            id="search"
+            name="search"
+            placeholder="Search all the GIFs and Stickers"
+            required
+            type="search"
+            value={input}
+            onChange={(e) => handleChange(e)}
           />
         </div>
         <button type="submit" title="Search" className={styles.formBtn}>
