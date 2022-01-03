ca522532921bd2e62a5589d860f21a41efff9ea5
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-01-03 10:28:52 -0400

    Update

--- client/src/routes/Detail/Detail.tsx
+++ client/src/routes/Detail/Detail.tsx
@@ -24,6 +24,7 @@ type Gif = {
 export default function Detail() {
   const { state } = useLocation();
   const { id, title, type, images } = state as Gif;
+  console.log("state", state);
 
   const searchResults = useSelector((state: RootStateOrAny) => state.search);
   const dispatch = useDispatch();
--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,16 +1,14 @@
 import styles from "./Home.module.scss";
 import { useEffect, useState } from "react";
 import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
-// Components
-import Banner from "../../components/Banner/Banner";
-import Loader from "../../components/Loader/Loader";
-// Icons
-import { HiTrendingUp } from "react-icons/hi";
-import { MdOutlineStickyNote2 } from "react-icons/md";
 import {
   getTrendingGifs,
   getTrendingStickers,
 } from "../../redux/actions/trending";
+// Components
+import Banner from "../../components/Banner/Banner";
+import Loader from "../../components/Loader/Loader";
+import Preview from "../../components/Preview/Preview";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -18,7 +16,6 @@ export default function Home() {
   const dispatch = useDispatch();
   const trending = useSelector((state: RootStateOrAny) => state.trending);
 
-  console.log("trending", trending);
   useEffect(() => {
     dispatch(getTrendingGifs(20));
     dispatch(getTrendingStickers(20));
@@ -30,59 +27,20 @@ export default function Home() {
       <Banner />
       {isLoading && <Loader />}
 
-      <section className={styles.section}>
-        <h2 className={styles.title}>
-          <HiTrendingUp />
-          Trending
-        </h2>
-        {/* <div className={styles.row}>
-          {trending.gifs.slice(0,6).map((gif: any) => (
-            <img
-              key={gif.id}
-              src={gif.images.medium}
-              alt={gif.title}
-              className={styles.gif}
-            />
-          ))}
-        </div> */}
-        <div className={styles.row}>
-          {trending.gifs.slice(0).map((gif: any) => (
-            <img
-              key={gif.id}
-              src={gif.images.medium}
-              alt={gif.title}
-              className={styles.gif}
-            />
-          ))}
-        </div>
-      </section>
-
-      <section className={styles.section}>
-        <h2 className={styles.title}>
-          <MdOutlineStickyNote2 />
-          Stickers
-        </h2>
-        {/* <div className={styles.row}>
-          {trending.gifs.slice(0,6).map((gif: any) => (
-            <img
-              key={gif.id}
-              src={gif.images.medium}
-              alt={gif.title}
-              className={styles.gif}
-            />
-          ))}
-        </div> */}
-        <div className={styles.row}>
-          {trending.stickers.slice(0).map((sticker: any) => (
-            <img
-              key={sticker.id}
-              src={sticker.images.medium}
-              alt={sticker.title}
-              className={styles.gif}
-            />
-          ))}
-        </div>
-      </section>
+      <Preview
+        trending={trending}
+        icon="trending"
+        title="Trending"
+        type="gifs"
+        link="/search/trending"
+      />
+      <Preview
+        trending={trending}
+        icon="stickers"
+        title="Stickers"
+        type="stickers"
+        link="/search/stickers"
+      />
     </main>
   );
 }
