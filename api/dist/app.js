6507f13c2a17f6261a30dc4329bb9ef35bce393b
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-01-09 14:28:30 -0400

    Update CORS config

--- api/dist/app.js
+++ api/dist/app.js
@@ -8,9 +8,11 @@ const index_1 = (0, tslib_1.__importDefault)(require("./routes/index"));
 const server = (0, express_1.default)();
 const port = process.env.PORT || 4000;
 // Middlewares
-server.use((0, cors_1.default)());
+server.use((0, cors_1.default)({
+    origin: "https://gipho.vercel.app/",
+}));
 server.use(express_1.default.json());
 server.use((0, morgan_1.default)("dev"));
 server.use("/", index_1.default);
-server.listen(port, () => console.log("Server is running on port 4000"));
+server.listen(port, () => console.log(`Server is running on port ${port}`));
 exports.default = server;
